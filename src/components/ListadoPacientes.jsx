4fb701db064e317579865e6d49ae7359ad5edf0d
Axel <axelomar.a.v@gmail.com>
2023-01-27 20:54:09 -0600

    Deployment

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,46 @@
+
+import usePacientes from '../hooks/usePacientes'
+import Paciente from './Paciente';
+const ListadoPacientes = () => {
+ 
+  const { pacientes } = usePacientes()
+  
+
+  return (
+    <>
+    
+      { pacientes.length ? (
+        <>
+             <h2 className='font-black text-3xl text-center'>Listado de Pacientes</h2>
+
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Adminstra tus  {''}
+            <span className='text-indigo-600 font-bold'> Pacientes y Citas</span>
+          </p>
+          {pacientes.map(paciente => (
+            <Paciente
+              key = {paciente._id}
+              paciente={paciente}
+
+            />
+          ))}
+        
+        </>
+      ) : (
+        <>
+          <h2 className='font-black text-3xl text-center'>No hay Pacientes</h2>
+
+          <p className='text-xl mt-5 mb-10 text-center'>
+            Comienza Agregando pacientes {''}
+            <span className='text-indigo-600 font-bold'> y apareceran en este lugar</span>
+          </p>
+        
+        </>
+      )}
+    
+    
+    </>
+  )
+}
+
+export default ListadoPacientes
---
+++ src/paginas/CambiarPassword.jsx
@@ -0,0 +1,89 @@
+import {useState} from 'react'
+import AdminNav from '../components/AdminNav'
+import Alerta from '../components/Alerta'
+import useAuth from '../hooks/useAuth'
+const CambiarPassword = () => {
+  const {guardarPassword} = useAuth()
+  const [alerta,setAlerta] = useState({})
+  const [password,setPassword] = useState({
+    pwd_actual:'',
+    pwd_nuevo:''
+  })
+  const handleSubmit = async  e=>{
+    e.preventDefault()
+   if(Object.values(password).some(campo => campo ==='')){
+    setAlerta({
+      msg:'Todos los campos son obligatoriso',
+      error:true
+    })
+    return
+   }
+   if(password.pwd_nuevo.length<6 ){
+    setAlerta({
+      msg:'El password debe contener minimo 6 caracteres',
+      error:true
+    })
+    return
+   }
+   const respuesta = await guardarPassword(password)
+   setAlerta(respuesta)
+  }
+  const {msg} = alerta
+  return (
+   <>
+        <AdminNav/>
+    
+
+        <h2 className='text-xl font-bold text-center mt-10'>Cambiar Password</h2>
+        <p className='text-xl mt-5 mb-10 text-center ' >Modifica tu {''} <span className='text-indigo-500 font-bold'>Password aqui</span></p>
+
+        <div className=' flex justify-center'>
+                <div className=' w-full md:w-1/2 bg-white shadow rounded-lg p-5'>
+                {msg && <Alerta
+                    alerta={alerta}
+                    />}
+                    <form onSubmit={handleSubmit}>
+                            <div className=' my-3'>
+                                <label className='uppercase font-bold text-gray-500'>Contraseña Actual</label>
+                                <input 
+                                    type="password"
+                                    className='border bg-gray-50 w-full p-2 mt-5 rounded-lg'
+                                    name='pwd_actual'
+                                    placeholder='Escribe tu cotraseña actual'
+                                    onChange={e=>setPassword({
+                                      ...password,
+                                      [e.target.name] : e.target.value
+                                    })}
+                                />
+                            </div>
+                            <div className=' my-3'>
+                                <label className='uppercase font-bold text-gray-500'>Nueva Contraseña</label>
+                                <input 
+                                    type="password"
+                                    className='border bg-gray-50 w-full p-2 mt-5 rounded-lg'
+                                    name='pwd_nuevo'
+                                    placeholder='Escribe tu nueva contraseña'
+                                    onChange={e=>setPassword({
+                                      ...password,
+                                      [e.target.name] : e.target.value
+                                    })}
+                                />
+                            </div>
+                            <input 
+                            type="submit"
+                            value="Guardar Cambios"
+                            className='bg-indigo-700 px-10  py-3 uppercase font-bold text-white rounded-lg  p-2 mt-5 w-full'
+                            
+                            />
+                    </form>
+
+
+                </div>
+
+        </div>
+
+   </>
+  )
+}
+
+export default CambiarPassword
